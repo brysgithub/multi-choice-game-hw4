97af5e4ef0bfd6e9aaa6e55c082d289424c9b4df
goblin.brn <bschenck0@gmail.com>
2021-11-29 15:34:45 -0800

    added function to display question and answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 // target needed elements 
 var startButtonEl = document.getElementById("start-btn");
-var timerEl = document.getElementById("timer")
-var questionHeading = document.getElementById("game-header");
-var questionText = document.getElementById("game-text");
+var timerEl = document.getElementById("timer");
+var gameBoxEl = document.getElementById("game-box");
+var questionHeadingEl = document.getElementById("game-header");
+var questionTextEl = document.getElementById("game-text");
 // var questionBtn1El = document.getElementById("questionBtn1");
 // var questionBtn2El = document.getElementById("questionBtn2");
 // var questionBtn3El = document.getElementById("questionBtn3");
 // var questionBtn4El = document.getElementById("questionBtn4");
-var questionBtn;
+// var questionBtn;
 var selectedQuestion;
+// var questionBtn1
+// var questionBtn2
+// var questionBtn3
+// var questionBtn4
 
 var timeLeft;
 
@@ -16,6 +21,7 @@ var timeLeft;
 
 var questionArray = [{
     question: "Question1",
+        questionTitle: ["question title"],
         multipleChoiceOptions: [
             "wrong1",
             "wrong2",
@@ -24,26 +30,26 @@ var questionArray = [{
         ],
         correct: "right",
 },
-{
-    question: "Question2",
-        multipleChoiceOptions: [
-            "wrong1",
-            "wrong2",
-            "right",
-            "wrong3",
-        ],
-        correct: "right",
-},
-{
-    question: "Question3",
-        multipleChoiceOptions: [
-            "wrong1",
-            "wrong2",
-            "right",
-            "wrong3",
-        ],
-        correct: "right",
-    }
+// {
+//     question: "Question2",
+//         multipleChoiceOptions: [
+//             "wrong1",
+//             "wrong2",
+//             "right",
+//             "wrong3",
+//         ],
+//         correct: "right",
+// },
+// {
+//     question: "Question3",
+//         multipleChoiceOptions: [
+//             "wrong1",
+//             "wrong2",
+//             "right",
+//             "wrong3",
+//         ],
+//         correct: "right",
+//     }
 ];
 
 // event listeners 
@@ -53,17 +59,21 @@ startButtonEl.addEventListener("click", startGame);
 
 // V functions of funcionality V
 
-// start button starting all tthe things
+// start button starting all the things
 function startGame () {
     console.log("Game start!");
     startButtonEl.style.display = "none";
-    questionHeading.innerHTML = "";
-    questionText.innerHTML = "";
+    questionHeadingEl.innerHTML = "";
+    questionTextEl.innerHTML = "";
     startTimer();
     displayQuestion();
+
+    // if (chosenAnswer === selectedQuestion.correct[0]) {
+    //     console.log("Correct!");
+    // }
 };
 
-// starts timer. crazy right?
+// starts timer
 function startTimer () {
     console.log("Timer start!")
     
@@ -88,13 +98,37 @@ function startTimer () {
 function displayQuestion () {
     console.log("Display question!")
 
-    selectQuestion(questionArray);
-    // questionBtn.innerHTML = [0];
+    chooseQuestion(questionArray);
+
+    // write title and question
+    questionHeadingEl.innerHTML = selectedQuestion.questionTitle;
+    questionTextEl.innerHTML = selectedQuestion.question;
+
+    //add buttons - TODO loop single section to procedurally add buttons -
+    var questionBtn1 = document.createElement("button");
+    questionBtn1.className = "question-btn";
+    gameBoxEl.appendChild(questionBtn1);
+    questionBtn1.innerHTML = selectedQuestion.multipleChoiceOptions[0];
+
+    var questionBtn2 = document.createElement("button");
+    questionBtn2.className = "question-btn";
+    gameBoxEl.appendChild(questionBtn2);
+    questionBtn2.innerHTML = selectedQuestion.multipleChoiceOptions[1];
+
+    var questionBtn3 = document.createElement("button");
+    questionBtn3.className = "question-btn";
+    gameBoxEl.appendChild(questionBtn3);
+    questionBtn3.innerHTML = selectedQuestion.multipleChoiceOptions[2];
+
+    var questionBtn4 = document.createElement("button");
+    questionBtn4.className = "question-btn";
+    gameBoxEl.appendChild(questionBtn4);
+    questionBtn4.innerHTML = selectedQuestion.multipleChoiceOptions[3];
 
 };
 
 // make for loop to sequentially select questions
-function selectQuestion (array) {
+function chooseQuestion (array) {
     // var selectedQuestion;
     for (let i = 0; i < array.length; i++) {
         selectedQuestion = array[i];
@@ -102,9 +136,13 @@ function selectQuestion (array) {
     } return selectedQuestion;
 };
 
+// end game
+function displayScore (){};
+
 function endGame () {
     if (timeLeft === 0) {
-
+        console.log("Game Over!");
+        displayScore();
     }
 };
 
